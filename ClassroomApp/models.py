fa17c2ea135d93581e2f6d22cb30c9d43d0ca178
HamideT4 <hamidemahamat20@gmail.com>
2023-02-28 12:49:44 +0100

    Ajout .gitignore

---
+++ ClassroomApp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+class Groupe(models.Model):
+    nom = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nom
+
+class Etudiant(models.Model):
+    Genre = (
+        ('M', ('Masculin')),
+        ('F', ('Feminin')),
+    )
+    nom = models.CharField(max_length=250)
+    prenom = models.CharField(max_length=200)
+    genre = models.CharField(max_length=32, choices=Genre)
+    email = models.CharField(max_length=250)
+    telephone = models.CharField(max_length=20, blank=True)
+    groupe = models.ForeignKey(Groupe, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return self.nom
+
+class Commande(models.Model):
+    Menu = (
+        ('Haricot', ('Haricot')),
+        ('Riz', ('Riz')),
+        ('Pâtes', ('Pâtes')),
+        ('Soupe', ('Soupe')),
+        ('Aucun', ('Aucun')),
+    )
+    menu = models.CharField(max_length=200, choices=Menu, default="", blank=True)
+    quantite = models.IntegerField(default=0)
+    groupe = models.ForeignKey(Groupe, on_delete=models.CASCADE, blank=True)
+    etudiant = models.OneToOneField(Etudiant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.menu
