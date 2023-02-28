54a127982823eb728ec528ac0cee3603bf0f758a
HamideT4 <hamidemahamat20@gmail.com>
2023-02-28 14:18:31 +0100

    migrations + cretion de urls.py

--- ClassroomApp/admin.py
+++ ClassroomApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import*
 
-# Register your models here.
+@admin.register(Etudiant)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'genre','telephone', 'email', )
+    ordering = ('nom', )
+    search_fields =  ('nom', )
+
+@admin.register(Groupe)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('nom', )
+    ordering = ('nom', )
+    search_fields =  ('nom', )
+
+@admin.register(Commande)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('menu','quantite', )
+    ordering = ('menu', )
+    search_fields =  ('menu', )
